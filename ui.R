7f28b459ee5f9e51388a14b42266e8ffad592f44
Jverma <j.verma5@gmail.com>
2013-11-29 01:04:35 -0600

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+
+	#Appl title
+	headerPanel("A/B Test Analysis"),
+	
+	sidebarPanel(
+	selectInput("variable", "Significance at Confidence",
+	list("95%" = "95",
+	"99%" = "99")), 
+	
+	numericInput("Avisitors", "Number of Control Visitors:", 1000),
+	numericInput("Bvisitors", "Number of Variation Visitors:", 1000),
+	numericInput("Aconversions", "Conversions in Contol Case:", 200),
+	numericInput("Bconversions", "Conversions in Variation Case:", 200),
+	
+	submitButton("Statistical Analysis")
+
+	),
+	
+	mainPanel(
+	h3(textOutput("caption")),
+		verbatimTextOutput("Confidence_intervalA"),
+		verbatimTextOutput("Confidence_intervalB"),
+		verbatimTextOutput("Zscore"),
+		verbatimTextOutput("pvalue")
+	)
+	
+))
